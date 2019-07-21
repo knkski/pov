3b8032443cdc7f7f7d8b75e9045fb5f565937776
Kenneth Koski <kennethkoski@gmail.com>
2019-07-21 14:58:00 -0500

    Mario pov saber

--- sabre.py
+++ sabre.py
@@ -1,37 +1,20 @@
-import time
 import board
-import busio
-
-import adafruit_lsm303
 import adafruit_dotstar as dotstar
+from bmp_reader import BMPReader
 
-dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=1.0, auto_write=False)
+image = BMPReader('mario.bmp')
+grid = image.get_pixels()
 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_lsm303.LSM303(i2c)
+dot = dotstar.DotStar(board.SCK, board.MOSI, 144, brightness=1.0, auto_write=False)
 
-old = (None, None, None)
-
-ON = True
-
+column = 0
 while True:
-    x, y, z = sensor.raw_acceleration
+    # Make image 4x bigger
+    for row in range(image.height):
+        for i in range(4):
+            dot[row*4 + i] = grid[row][column]
+            dot[143 - (row*4 + i)] = grid[row][column]
+    dot.show()
 
-    if [o for o in old if o is not None]:
-        diff = (abs(old[0] - x), abs(old[1] - y), abs(old[2] - z))
-
-        dot.brightness = max(diff) / 32768.
-
-        if ON:
-            dot[0] = (255, 0, 0)
-        else:
-            dot[0] = (0, 0, 0)
-        dot.show()
-
-        ON = not ON
-
-        old = (x, y, z)
-        time.sleep(0.1)
-    else:
-        # print('Debug: (%s, %s, %s)' % (x, y, z))
-        old = (x, y, z)
+    column += 1
+    column %= image.width
